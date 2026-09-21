beeaa868efdb3e86abb72e2699dfbd2ee9fd243d
shunshine-strawberry <bedayoshunshine226@gmail.com>
2026-09-21 21:25:58 +0800

    orderstat

--- src/customer/pages/order-status/order-status.jsx
+++ src/customer/pages/order-status/order-status.jsx
@@ -8,25 +8,21 @@ import Time from "/src/assets/icons/time.svg?react";
 
 const deliveryStatuses = [
     "Order Received",
-    "Preparing",
-    "Ready for Delivery",
-    "Out for Delivery",
-    "Delivered",
+    "Preparing Your Order",
+    "Done Preparing",
+    "Completed",
 ];
 
 const pickupStatuses = [
-    "Order Received",
-    "Preparing",
-    "Ready for Pickup",
+    "Preparing Your Order",
 ];
 
 const statusMessages = {
     "Order Received": "We got your order!",
-    "Preparing": "Our team is brewing and cooking.",
-    "Ready for Pickup": "Your order is ready for pickup!",
-    "Ready for Delivery": "Your order is ready for delivery!",
-    "Out for Delivery": "Your order is on the way!",
-    "Delivered": "Enjoy your order!",
+    "Preparing Your Order": "Our team is brewing and cooking.", 
+    "Done Preparing": "Your order is ready for pickup/delivery!",
+    "Completed": "Enjoy your order!",
+
 };
 
 function formatAmount(value) {
